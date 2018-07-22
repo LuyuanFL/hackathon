30acab23460b99289ca876e85753d735b27cd13e
LuyuanFL <37862208+LuyuanFL@users.noreply.github.com>
2018-07-22 09:27:24 +0800

    Add files via upload

---
+++ aruiduino光敏检测带复位/sketch_jul21a.ino
@@ -0,0 +1,19 @@
+#define AD5 A5
+#define AD4 A4
+int Intensity=0;
+void setup() {
+// put your setup code here, to run once:
+Serial.begin(9600);
+}
+void loop() {
+// put your main code here, to run repeatedly:
+if(analogRead(AD4)==0)
+{
+Serial.println(9999);
+delay(3000);
+}
+Intensity =analogRead(AD5)+1000;
+Serial.println(Intensity);
+  delay(100);
+
+}
